29ea10ca0f8bd05e4ba1a57183b915aaa7a4cb8c
Megan Potter <mzp@google.com>
2021-05-14 15:39:15 -0400

    chore: update to latest node pub/sub library

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-nodejs:0.0.7")
 
     // Normally we'd want Dukat to generate typings for Kotlin, but it fails on this package.
-    implementation(npm("@google-cloud/pubsub", "^2.7.0" /*, generateExternals = true*/))
+    implementation(npm("@google-cloud/pubsub", "^2.12.0" /*, generateExternals = true*/))
 }
 
 kotlin {
